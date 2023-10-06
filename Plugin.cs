ce8f83cedf347266739db49cd54e97fc287e16b6
DerpyChap <holla@derpychap.co.uk>
2023-10-06 16:44:05 +0100

    Hat.

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Hat
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            _harmony.PatchAll();
+        }
+
+        internal new static ManualLogSource Logger { get; set; }
+    }
+
+    [HarmonyPatch(typeof(GameController), nameof(GameController.loadAssetBundleResources))]
+    class GameControllerStartPatch
+    {
+        [HarmonyPrefix]
+        static void PatchHat()
+        {
+            GlobalVariables.chosen_hat = 1;
+            Plugin.Logger.LogInfo("Hat patched!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TromboneChamp-Hat
-Hat.
+# Hat
+Re-enables Trombone Champ's Hat feature from the April 1st 2022 update.
